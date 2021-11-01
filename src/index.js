f292295c165370896922c02a4890a9dfd10cd8fe
Roie Schwaber-Cohen <roie.cohen@gmail.com>
2021-11-01 15:07:07 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import { Auth0Provider } from '@auth0/auth0-react'
+import { AsertoProvider } from '@aserto/aserto-react'
+import config from './utils/config'
+import history from './utils/history'
+import { UsersProvider } from './utils/users'
+
+// import bootstrap, font-awesome
+import 'bootstrap/dist/css/bootstrap.css'
+import 'font-awesome/css/font-awesome.min.css'
+
+// import local styles after default styles so they take precedence
+import './theme.css'
+import './index.css'
+
+const onRedirectCallback = async (appState) => {
+  history.push(
+    appState && appState.returnTo !== '/'
+      ? appState.returnTo
+      : '/people' //window.location.pathname
+  );
+};
+
+ReactDOM.render(
+  <Auth0Provider
+    domain={config.domain}
+    clientId={config.clientId}
+    audience={config.audience}
+    redirectUri={window.location.origin}
+    onRedirectCallback={onRedirectCallback}
+  >
+    <AsertoProvider>
+      <UsersProvider>
+        <App />
+      </UsersProvider>
+    </AsertoProvider>
+  </Auth0Provider>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ src/utils/config.js
@@ -0,0 +1,26 @@
+const configuration = {
+  domain: process.env.REACT_APP_DOMAIN,
+  clientId: process.env.REACT_APP_CLIENT_ID,
+  audience: process.env.REACT_APP_AUDIENCE,
+  policyRoot: process.env.REACT_APP_POLICY_ROOT,
+  apiOrigin: process.env.REACT_APP_API_ORIGIN
+}
+
+if (!process.env.REACT_APP_NETLIFY) {
+  if (!configuration.apiOrigin) {
+    const url = new URL(window.location.origin);
+    if (url.port === '3000') {
+      url.port = '3001';
+    }
+    const urlstring = url + '';
+    configuration.apiOrigin = 
+      urlstring.substr(-1) === '/' ? 
+      urlstring.substr(0, urlstring.length - 1) : 
+      urlstring;
+  }
+} else {
+  configuration.apiOrigin = `${window.location.origin}/.netlify/functions/api-server`;
+}
+
+export default configuration
+
